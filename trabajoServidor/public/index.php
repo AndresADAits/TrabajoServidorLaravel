baf78668459534c15abb427526c075cfdc9d637f
AndresADAits <45296894+AndresADAits@users.noreply.github.com>
2020-01-21 17:07:56 +0100

    seguimos

--- trabajoServidor/public/index.php
+++ trabajoServidor/public/index.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   José Andrés Fernández <joanferfra@gmail.com>
  */
 
 define('LARAVEL_START', microtime(true));
